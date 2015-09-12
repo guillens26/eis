238d6de5a1630033e95492e4f7c55a66c5534d2f
Guillermo <guillens91@gmail.com>
2015-09-12 04:20:17 -0300

    Refactor code

--- Kata1-Chopper/model/chopper.rb
+++ Kata1-Chopper/model/chopper.rb
@@ -1,28 +1,44 @@
 class Chopper
 
-  def chop(int,array)
+  def chop(int, array)
+    #Calculo el tamaño de mi arreglo y le resto 1
     array.length - 1
   end
 
-  def sum(array)  
-    numbers = ['cero', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve']
-    resultado = []
+  #Creo numbers para luego traducir el numero a palabras 
+  $numbers = ['cero', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve']
+  
+  def sum(array) 
+    # Si el array esta vacio devuelvo 'vacio' 
     if(array == [])
       'vacio'
     else
+      #Calculo el total del contenido de mis posisiones del arreglo sumando pos x pos
       total = 0
       array.each do |elem|
         total += elem 
       end
-      if (total<9)    
-        numbers[total]
-      elsif(total<100)
-        resultado << (total - total/10 * 10)
-        resultado << total = total/10
-        numbers[resultado[1]]+ ',' + numbers[resultado[0]]
-      else
-        'demasiado grande'
-      end
+      #Imprimo el numero total en palabras
+      print_int_in_words(total)
     end
   end 
+
+  def print_int_in_words(int)
+    num = int
+    # Creo result para guardar los resultados de 2 digitos
+    result = []
+    #Pregunto para saber si mi entero es de 1 digito
+    if (num<9)    
+      $numbers[num]
+    #Pregunto para saber si mi entero es de mas de dos digitos sino calculo
+    elsif(int<100)
+      result << (num - num/10 * 10)
+      result << num = num/10
+      $numbers[result[1]]+ ',' + $numbers[result[0]]
+    # Si es de 3 digitos devuelvo 'demasiado grande'
+    else
+      'demasiado grande'
+    end
+  end
+
 end
