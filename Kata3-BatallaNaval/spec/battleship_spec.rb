03895a3930762eb06da12525ec805e31be7edfb6
Guillermo <guillens91@gmail.com>
2015-10-13 03:18:50 -0300

    Add Battleship

---
+++ Kata3-BatallaNaval/spec/battleship_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec' 
+require_relative '../model/battleship'
+
+describe 'Battleship' do
+
+  let(:battleship) { Battleship.new} 
+
+end
--- Kata3-BatallaNaval/spec/una_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rspec'
-# require_relative '../model/algun_archivo'
-
-
